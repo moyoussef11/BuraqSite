4cbeb01cc5a21563abdd72d1c369c84fb5559884
moyoussef11 <mohmedyoussef472@gmail.com>
2024-04-13 21:39:40 +0200

    deploy project

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+// toggle
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(
+    "[data-collapse-toggle='mobile-menu-2']"
+  );
+  const menu = document.getElementById("mobile-menu-2");
+
+  button.addEventListener("click", function () {
+    const expanded = button.getAttribute("aria-expanded") === "true" || false;
+    button.setAttribute("aria-expanded", !expanded);
+    menu.classList.toggle("hidden");
+  });
+});
+
+
+
+$(document).ready(
+  (function () {
+    $(".client-single").on("click", function (event) {
+      event.preventDefault();
+
+      var active = $(this).hasClass("active");
+
+      var parent = $(this).parents(".testi-wrap");
+
+      if (!active) {
+        var activeBlock = parent.find(".client-single.active");
+
+        var currentPos = $(this).attr("data-position");
+
+        var newPos = activeBlock.attr("data-position");
+
+        activeBlock
+          .removeClass("active")
+          .removeClass(newPos)
+          .addClass("inactive")
+          .addClass(currentPos);
+        activeBlock.attr("data-position", currentPos);
+
+        $(this)
+          .addClass("active")
+          .removeClass("inactive")
+          .removeClass(currentPos)
+          .addClass(newPos);
+        $(this).attr("data-position", newPos);
+      }
+    });
+  })(jQuery)
+);
