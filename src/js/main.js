bbc625a2f4f05678ccbdb6baf4e1d04b8dbb285f
moyoussef11 <mohmedyoussef472@gmail.com>
2024-04-14 23:11:00 +0200

    some change

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.toggle("hidden");
   });
 });
-
-
+// AOS
+AOS.init();
 
 $(document).ready(
   (function () {
@@ -47,3 +47,13 @@ $(document).ready(
     });
   })(jQuery)
 );
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
